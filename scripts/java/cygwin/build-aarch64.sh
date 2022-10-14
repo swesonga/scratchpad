93fde13bb80b1f19c5098730da20ab7343043c28
Saint Wesonga <sawesong@microsoft.com>
2022-10-13 23:18:04 -0600

    Enable building for customizable architecture in one script

--- scripts/java/cygwin/build-aarch64.sh
+++ scripts/java/cygwin/build-aarch64.sh
@@ -2,18 +2,28 @@
 #
 # Execute in Cygwin after configuring the repo.
 # A boot JDK may be required if building the latest code from upstream.
+# Download JDKs from https://adoptium.net/temurin/releases/?version=19
 #
-# Debug:
+# x86_64 Debug:
+#  bash configure --with-debug-level=slowdebug --with-boot-jdk=/cygdrive/d/dev/Software/java/jdk-19+36
+#  bash configure --with-debug-level=slowdebug --with-boot-jdk=/cygdrive/d/dev/Software/java/jdk-19+36  --with-hsdis=llvm --with-llvm=/cygdrive/d/dev/software/llvm-x86_64/
+#
+# x86_64 Release:
+#  bash configure --with-boot-jdk=/cygdrive/d/dev/Software/java/jdk-19+36
+#  bash configure --with-boot-jdk=/cygdrive/d/dev/Software/java/jdk-19+36  --with-hsdis=llvm --with-llvm=/cygdrive/d/dev/software/llvm-x86_64/
+#
+# aarch64 Debug:
 #  bash configure --openjdk-target=aarch64-unknown-cygwin --with-debug-level=slowdebug --with-boot-jdk=/cygdrive/d/dev/repos/java/infra/binaries/jdk-18.0.2
 #  bash configure --openjdk-target=aarch64-unknown-cygwin --with-debug-level=slowdebug --with-boot-jdk=/cygdrive/d/dev/repos/java/infra/binaries/jdk-18.0.2  --with-hsdis=llvm --with-llvm=/cygdrive/d/dev/software/llvm-aarch64/
+#  bash configure --openjdk-target=aarch64-unknown-cygwin --with-debug-level=slowdebug --with-boot-jdk=/cygdrive/d/dev/repos/java/forks/jdk/build/windows-x86_64-server-slowdebug/jdk  --with-hsdis=llvm --with-llvm=/cygdrive/d/dev/software/llvm-aarch64/
 #
-# Release:
+# aarch64 Release:
 #  bash configure --openjdk-target=aarch64-unknown-cygwin --with-boot-jdk=/cygdrive/d/dev/repos/java/infra/binaries/jdk-18.0.2
 #  bash configure --openjdk-target=aarch64-unknown-cygwin --with-boot-jdk=/cygdrive/d/dev/repos/java/infra/binaries/jdk-18.0.2  --with-hsdis=llvm --with-llvm=/cygdrive/d/dev/software/llvm-aarch64/
 #
 # Run this script as follows:
 #
-#  time /cygdrive/d/dev/repos/scratchpad/scripts/java/cygwin/build-aarch64.sh
+#  time /cygdrive/d/dev/repos/scratchpad/scripts/java/cygwin/build-aarch64.sh /cygdrive/c/shared/java/builds/
 #
 
 # Exit immediately if a command exits with a non-zero status. See
@@ -30,40 +40,42 @@ function log_message()
 timestamp=`date +%Y-%m-%d_%H%M-%S`
 
 # TODO: make these configurable
-arch=aarch64
+# Select either the x86_64 or aarch64 architectures
+arch=x86_64
 os=windows
 build_hsdis=1
 debug_level=slowdebug
-llvm_path=/cygdrive/d/dev/software/llvm-aarch64
+llvm_path=/cygdrive/d/dev/software/llvm-$arch
 log_root="build/mylogs"
 # use "debug" for a more detailed log
 log_verbosity=cmdlines
 redirect_output=1
+build_conf="${os}-${arch}-server-${debug_level}"
 
 log_message "Starting $debug_level build with timestamp $timestamp for OS type $OSTYPE"
 mkdir -p $log_root
 
-images_log="$log_root/images-${debug_level}-${timestamp}.txt"
-jtreg_native_log="$log_root/test-${debug_level}-${timestamp}.txt"
+images_log="$log_root/images-${build_conf}-${timestamp}.txt"
+jtreg_native_log="$log_root/test-${build_conf}-${timestamp}.txt"
 
-images_zip="jdk-${arch}-${debug_level}-${timestamp}.zip"
-test_zip="test-${arch}-${debug_level}-${timestamp}.zip"
+images_zip="jdk-${build_conf}-${timestamp}.zip"
+test_zip="test-${build_conf}-${timestamp}.zip"
 
 log_message "Building images"
 if [ $redirect_output -ne 0 ]; then
-    make images CONF=$debug_level LOG=$log_verbosity > $images_log
+    make images CONF=$build_conf LOG=$log_verbosity > $images_log
 else
-    make images CONF=$debug_level LOG=$log_verbosity
+    make images CONF=$build_conf LOG=$log_verbosity
 fi
 
 log_message "Building jtreg native binaries"
 if [ $redirect_output -ne 0 ]; then
-    make build-test-jdk-jtreg-native CONF=$debug_level LOG=$log_verbosity > $jtreg_native_log
+    make build-test-jdk-jtreg-native CONF=$build_conf LOG=$log_verbosity > $jtreg_native_log
 else
-    make build-test-jdk-jtreg-native CONF=$debug_level LOG=$log_verbosity
+    make build-test-jdk-jtreg-native CONF=$build_conf LOG=$log_verbosity
 fi
 
-built_jdk="build/${os}-${arch}-server-${debug_level}/jdk/"
+built_jdk="build/${build_conf}/jdk/"
 
 if [ $build_hsdis -ne 0 ]; then
     hsdis_build_log="$log_root/hsdis_build-${timestamp}.txt"
@@ -71,16 +83,16 @@ if [ $build_hsdis -ne 0 ]; then
 
     log_message "Building hsdis"
     if [ $redirect_output -ne 0 ]; then
-        make build-hsdis CONF=$debug_level LOG=$log_verbosity > $hsdis_build_log
+        make build-hsdis CONF=$build_conf LOG=$log_verbosity > $hsdis_build_log
     else
-        make build-hsdis CONF=$debug_level LOG=$log_verbosity
+        make build-hsdis CONF=$build_conf LOG=$log_verbosity
     fi
 
     log_message "Installing hsdis"
     if [ $redirect_output -ne 0 ]; then
-        make install-hsdis CONF=$debug_level LOG=$log_verbosity > $hsdis_install_log
+        make install-hsdis CONF=$build_conf LOG=$log_verbosity > $hsdis_install_log
     else
-        make install-hsdis CONF=$debug_level LOG=$log_verbosity
+        make install-hsdis CONF=$build_conf LOG=$log_verbosity
     fi
 
     cp "${llvm_path}/bin/LLVM-C.dll" "${built_jdk}/bin"
