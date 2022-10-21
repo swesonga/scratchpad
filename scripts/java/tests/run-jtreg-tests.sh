96567a9eccdbb2fac941aada811d1677be799eea
Saint Wesonga <sawesong@microsoft.com>
2022-10-21 17:12:15 -0600

    Update list of tests

--- scripts/java/tests/run-jtreg-tests.sh
+++ scripts/java/tests/run-jtreg-tests.sh
@@ -10,25 +10,42 @@
 # Exit immediately if a command exits with a non-zero status. See
 # https://unix.stackexchange.com/questions/22726/how-to-conditionally-do-something-if-a-command-succeeded-or-failed
 # https://stackoverflow.com/questions/19622198/what-does-set-e-mean-in-a-bash-script
-set -e
+#set -e
+
+declare -a ui_tests=(
+ "test/jdk/java/awt/Focus/8000326/SetFocusTraversalKeysEnabledTest.java"
+ "test/jdk/java/awt/Focus/8282640/ScrollPaneFocusBugTest.java"
+ "test/jdk/javax/swing/JButton/4659800/SpaceKeyActivatesButton.java"
+ "test/jdk/javax/swing/JComboBox/JComboBoxPopupMenuEventTest.java"
+ "test/jdk/javax/swing/JFileChooser/JFileChooserSetLocationTest.java"
+ "test/jdk/javax/swing/JLabel/4138746/JLabelMnemonicsTest.java"
+ "test/jdk/javax/swing/JList/4618767/JListSelectedElementTest.java"
+ "test/jdk/javax/swing/JRootPane/DefaultButtonTest.java"
+ "test/jdk/javax/swing/JSpinner/4670051/DateFieldUnderCursorTest.java"
+ "test/jdk/javax/swing/JSplitPane/4164779/JSplitPaneKeyboardNavigationTest.java"
+ "test/jdk/javax/swing/JSplitPane/4615365/JSplitPaneDividerLocationTest.java"
+ "test/jdk/javax/swing/JSplitPane/4820080/JSplitPaneDragColorTest.java"
+ "test/jdk/javax/swing/JTextArea/4514331/TabShiftsFocusToNextComponent.java"
+ "test/jdk/javax/swing/JTextField/4532513/DefaultCaretRequestsFocusTest.java"
+ "test/jdk/javax/swing/JTree/4518432/JTreeNodeCopyPasteTest.java"
+ "test/jdk/javax/swing/JTree/4618767/JTreeSelectedElementTest.java"
+)
 
 declare -a java_tests=(
- "java/awt/Focus/8000326/SetFocusTraversalKeysEnabledTest.java"
- "java/awt/Focus/8282640/ScrollPaneFocusBugTest.java"
- "javax/swing/JButton/4659800/SpaceKeyActivatesButton.java"
- "javax/swing/JComboBox/JComboBoxPopupMenuEventTest.java"
- "javax/swing/JFileChooser/JFileChooserSetLocationTest.java"
- "javax/swing/JLabel/4138746/JLabelMnemonicsTest.java"
- "javax/swing/JList/4618767/JListSelectedElementTest.java"
- "javax/swing/JRootPane/DefaultButtonTest.java"
- "javax/swing/JSpinner/4670051/DateFieldUnderCursorTest.java"
- "javax/swing/JSplitPane/4164779/JSplitPaneKeyboardNavigationTest.java"
- "javax/swing/JSplitPane/4615365/JSplitPaneDividerLocationTest.java"
- "javax/swing/JSplitPane/4820080/JSplitPaneDragColorTest.java"
- "javax/swing/JTextArea/4514331/TabShiftsFocusToNextComponent.java"
- "javax/swing/JTextField/4532513/DefaultCaretRequestsFocusTest.java"
- "javax/swing/JTree/4518432/JTreeNodeCopyPasteTest.java"
- "javax/swing/JTree/4618767/JTreeSelectedElementTest.java"
+ "test/hotspot/jtreg/compiler/c2/ClearArray.java"
+ "test/hotspot/jtreg/compiler/c2/irTests/scalarReplacement/AllocationMergesTests.java"
+ "test/hotspot/jtreg/compiler/intrinsics/unsafe/HeapByteBufferTest.java#id1"
+ "test/hotspot/jtreg/compiler/loopopts/TestOverUnrolling2.java"
+ "test/hotspot/jtreg/compiler/loopopts/superword/TestPeeledReductionNode.java"
+ "test/hotspot/jtreg/compiler/rangechecks/TestRangeCheckCmpUUnderflow.java"
+ "test/hotspot/jtreg/vmTestbase/gc/gctests/LargeObjects/large002/TestDescription.java"
+ "test/jdk/java/nio/file/Files/probeContentType/Basic.java"
+ "test/jdk/security/infra/java/security/cert/CertPathValidator/certification/QuoVadisCA.java"
+ "test/langtools/tools/javac/6257443/T6257443.java"
+ "test/langtools/tools/javac/8074306/TestSyntheticNullChecks.java"
+ "test/langtools/tools/javac/StringConcat/TestIndyStringConcat.java"
+ "test/langtools/tools/javac/jvm/ClassRefDupInConstantPoolTest.java"
+ "test/langtools/tools/javac/warnings/suppress/PackageInfo.java"
 )
 
 if [ $# -lt 2 ]
@@ -49,8 +66,9 @@ declare -a missing_tests=()
 # https://stackoverflow.com/questions/8880603/loop-through-an-array-of-strings-in-bash
 for java_test in "${java_tests[@]}"
 do
-   java_test="test/jdk/${java_test}"
    if test -f $java_test ; then
+       echo -e "\n\n\n---- Running $java_test ----"
+
        $test_jdk/bin/java -Xmx512m -jar $jtreg_jar_path -agentvm -ignore:quiet -automatic -xml -vmoption:-Xmx512m -timeoutFactor:4 -concurrency:1 -testjdk:$test_jdk -verbose:fail,error,summary $java_test
    else
        missing_tests+=($java_test)
