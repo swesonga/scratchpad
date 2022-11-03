54b6b07abffcda93abd168cb633827bdd9e36541
Saint Wesonga <sawesong@microsoft.com>
2022-11-03 12:49:24 -0600

    Compute sum of HFA struct members using variadic and non-variadic functions

--- compilers/tests/aarch64/abi/floating-point/hfa.c
+++ compilers/tests/aarch64/abi/floating-point/hfa.c
@@ -26,10 +26,18 @@
 * Code for investigating MSVC ARM64 ABI code generation.
 * Derived from https://github.com/openjdk/jdk/blob/18cd16d2eae2ee624827eb86621f3a4ffd98fe8c/test/jdk/java/foreign/libVarArgs.c
 *
-*   Compile using
+*   Compile (unoptimized):
 *       cl /c hfa.c
 *       dumpbin /disasm /out:hfa.asm hfa.obj
 *       dumpbin /all /out:hfa.txt hfa.obj
+*
+*   Compile (optimized):
+*       cl /c /O2 /Fo"hfa-optimized.obj" hfa.c
+*       dumpbin /disasm /out:hfa-optimized.asm hfa-optimized.obj
+*       dumpbin /all /out:hfa-optimized.txt hfa-optimized.obj
+*
+* Variadic function documentation:
+*  - https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/va-arg-va-copy-va-end-va-start
 */
 
 #include <stdio.h>
@@ -46,36 +54,22 @@ struct S_DD { double p0; double p1; };
 struct S_DDD { double p0; double p1; double p2; };
 struct S_DDDD { double p0; double p1; double p2; double p3; };
 
-void func_S_F(int flag, struct S_F floats)
+/*
+* Struct with 1 float
+*/
+float func_S_F(int flag, struct S_F floats)
 {
+    return floats.p0;
 }
 
-void func_S_FF(int flag, struct S_FF floats)
+float variadic_func_S_F(int flag, ...)
 {
-}
-
-void func_S_FFF(int flag, struct S_FFF floats)
-{
-}
-
-void func_S_FFFF(int flag, struct S_FFFF floats)
-{
-}
-
-void func_S_D(int flag, struct S_D doubles)
-{
-}
-
-void func_S_DD(int flag, struct S_DD doubles)
-{
-}
-
-void func_S_DDD(int flag, struct S_DDD doubles)
-{
-}
-
-void func_S_DDDD(int flag, struct S_DDDD doubles)
-{
+    va_list argptr;
+    va_start(argptr, flag);
+    struct S_F floats = va_arg(argptr, struct S_F);
+    va_end(argptr);
+    
+    return floats.p0;
 }
 
 void call_S_F()
@@ -84,10 +78,58 @@ void call_S_F()
     func_S_F(0xCAFE0123, struct_float_1);
 }
 
+void call_variadic_S_F()
+{
+    struct S_F struct_float_1 = { 1234.56789012345 };
+    variadic_func_S_F(0xCAFE0123, struct_float_1);
+}
+
+/*
+* Struct with 2 floats
+*/
+float func_S_FF(int flag, struct S_FF floats)
+{
+    return floats.p0 + floats.p1;
+}
+
+float variadic_func_S_FF(int flag, ...)
+{
+    va_list argptr;
+    va_start(argptr, flag);
+    struct S_FF floats = va_arg(argptr, struct S_FF);
+    va_end(argptr);
+    
+    return floats.p0 + floats.p1;
+}
+
 void call_S_FF()
 {
     struct S_FF struct_float_2 = { 1234.56789012345, 240.0 };
     func_S_FF(0xCAFE1234, struct_float_2);
+}
+
+void call_variadic_S_FF()
+{
+    struct S_FF struct_float_2 = { 1234.56789012345, 240.0 };
+    variadic_func_S_FF(0xCAFE1234, struct_float_2);
+}
+
+/*
+* Struct with 3 floats
+*/
+float func_S_FFF(int flag, struct S_FFF floats)
+{
+    return floats.p0 + floats.p1 + floats.p2;
+}
+
+float variadic_func_S_FFF(int flag, ...)
+{
+    va_list argptr;
+    va_start(argptr, flag);
+    struct S_FFF floats = va_arg(argptr, struct S_FFF);
+    va_end(argptr);
+    
+    return floats.p0 + floats.p1 + floats.p2;
 }
 
 void call_S_FFF()
@@ -96,10 +138,58 @@ void call_S_FFF()
     func_S_FFF(0xCAFE2345, struct_float_3);
 }
 
+void call_variadic_S_FFF()
+{
+    struct S_FFF struct_float_3 = { 1234.56789012345, 2400.0, 1200.0 };
+    variadic_func_S_FFF(0xCAFE2345, struct_float_3);
+}
+
+/*
+* Struct with 4 floats
+*/
+float func_S_FFFF(int flag, struct S_FFFF floats)
+{
+    return floats.p0 + floats.p1 + floats.p2 + floats.p3;
+}
+
+float variadic_func_S_FFFF(int flag, ...)
+{
+    va_list argptr;
+    va_start(argptr, flag);
+    struct S_FFFF floats = va_arg(argptr, struct S_FFFF);
+    va_end(argptr);
+    
+    return floats.p0 + floats.p1 + floats.p2 + floats.p3;
+}
+
 void call_S_FFFF()
 {
     struct S_FFFF struct_float_4 = { 1234.56789012345, 24000.0, 12000.0, 3.14159265358979323846 };
     func_S_FFFF(0xCAFE4567, struct_float_4);
+}
+
+void call_variadic_S_FFFF()
+{
+    struct S_FFFF struct_float_4 = { 1234.56789012345, 24000.0, 12000.0, 3.14159265358979323846 };
+    variadic_func_S_FFFF(0xCAFE4567, struct_float_4);
+}
+
+/*
+* Struct with 1 double
+*/
+double func_S_D(int flag, struct S_D doubles)
+{
+    return doubles.p0;
+}
+
+double variadic_func_S_D(int flag, ...)
+{
+    va_list argptr;
+    va_start(argptr, flag);
+    struct S_D doubles = va_arg(argptr, struct S_D);
+    va_end(argptr);
+    
+    return doubles.p0;
 }
 
 void call_S_D()
@@ -108,10 +198,58 @@ void call_S_D()
     func_S_D(0xCAFE5678, struct_double_1);
 }
 
+void call_variadic_S_D()
+{
+    struct S_D struct_double_1 = { 1234.56789012345 };
+    variadic_func_S_D(0xCAFE5678, struct_double_1);
+}
+
+/*
+* Struct with 2 doubles
+*/
+double func_S_DD(int flag, struct S_DD doubles)
+{
+    return doubles.p0 + doubles.p1;
+}
+
+double variadic_func_S_DD(int flag, ...)
+{
+    va_list argptr;
+    va_start(argptr, flag);
+    struct S_DD doubles = va_arg(argptr, struct S_DD);
+    va_end(argptr);
+    
+    return doubles.p0 + doubles.p1;
+}
+
 void call_S_DD()
 {
     struct S_DD struct_double_2 = { 1234.56789012345, 240.0 };
     func_S_DD(0xCAFE6789, struct_double_2);
+}
+
+void call_variadic_S_DD()
+{
+    struct S_DD struct_double_2 = { 1234.56789012345, 240.0 };
+    variadic_func_S_DD(0xCAFE6789, struct_double_2);
+}
+
+/*
+* Struct with 3 doubles
+*/
+double func_S_DDD(int flag, struct S_DDD doubles)
+{
+    return doubles.p0 + doubles.p1 + doubles.p2;
+}
+
+double variadic_func_S_DDD(int flag, ...)
+{
+    va_list argptr;
+    va_start(argptr, flag);
+    struct S_DDD doubles = va_arg(argptr, struct S_DDD);
+    va_end(argptr);
+    
+    return doubles.p0 + doubles.p1 + doubles.p2;
 }
 
 void call_S_DDD()
@@ -120,10 +258,40 @@ void call_S_DDD()
     func_S_DDD(0xCAFE7890, struct_double_3);
 }
 
+void call_variadic_S_DDD()
+{
+    struct S_DDD struct_double_3 = { 1234.56789012345, 2400.0, 1200.0 };
+    variadic_func_S_DDD(0xCAFE7890, struct_double_3);
+}
+
+/*
+* Struct with 4 doubles
+*/
+double func_S_DDDD(int flag, struct S_DDDD doubles)
+{
+    return doubles.p0 + doubles.p1 + doubles.p2 + doubles.p3;
+}
+
+double variadic_func_S_DDDD(int flag, ...)
+{
+    va_list argptr;
+    va_start(argptr, flag);
+    struct S_DDDD doubles = va_arg(argptr, struct S_DDDD);
+    va_end(argptr);
+    
+    return doubles.p0 + doubles.p1 + doubles.p2 + doubles.p3;
+}
+
 void call_S_DDDD()
 {
     struct S_DDDD struct_double_4 = { 1234.56789012345, 24000.0, 12000.0, 3.14159265358979323846 };
     func_S_DDDD(0xCAFE890A, struct_double_4);
+}
+
+void call_variadic_S_DDDD()
+{
+    struct S_DDDD struct_double_4 = { 1234.56789012345, 24000.0, 12000.0, 3.14159265358979323846 };
+    variadic_func_S_DDDD(0xCAFE890A, struct_double_4);
 }
 
 int main()
