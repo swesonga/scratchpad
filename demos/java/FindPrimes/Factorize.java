a9e5f978f00b27732d02126a77a93b01a45ca26e
Saint Wesonga <sawesong@microsoft.com>
2023-03-28 16:38:22 -0600

    Factorize even numbers &  use pow in divisor

--- demos/java/FindPrimes/Factorize.java
+++ demos/java/FindPrimes/Factorize.java
@@ -13,6 +13,11 @@
  *  java Factorize 438880205542
  *  java Factorize 43888020554297731
  *  java Factorize 4388802055429773100203726550535118822125
+ *
+ * BigInteger documentation:
+ *
+ *  https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/math/BigInteger.html
+ *
  */
 import java.math.BigInteger;
 
@@ -59,16 +64,17 @@ public class Factorize {
         }
 
         // Ingore possibility of invalid or negative numbers
-        BigInteger number;
+        BigInteger input, number;
 
         try {
-            number = new BigInteger(args[0]);
+            input = new BigInteger(args[0]);
         }
         catch (NumberFormatException nfe) {
-            System.err.println("The specified number must be a valid base 10 number.");
+            System.err.println("Error: " + args[0] + " is not a valid base 10 number.");
             return;
         }
 
+        number = input;
         if (number.compareTo(TWO) < 1) {
             System.err.println("The specified number must be greater than 2.");
             return;
@@ -77,34 +83,41 @@ public class Factorize {
         long factors = 0;
         int trailingZeros = countTrailingZeros(number);
         if (trailingZeros > 0) {
+            System.out.println("Found a factor: 2^{" + trailingZeros + "} of " + number);
             number = number.divide(TWO.pow(trailingZeros));
-            System.out.println("Found a factor: 2^{" + trailingZeros + "}");
-
-            if (number.compareTo(ONE) == 0) {
-                return;
-            }
 
             factors++;
         }
 
-        var sqrt = number.sqrt();
+        var inputSqrt = input.sqrt();
+        var sqrt = inputSqrt;
         BigInteger i = ONE.add(TWO);
 
-        while (true) {
+        while (i.compareTo(sqrt) <= 0) {
             if (number.remainder(i).compareTo(ZERO) == 0) {
                 factors++;
                 var maxPowerOfi = computeMaxPower(number, i);
                 System.out.println("Found a factor: " + i + "^{" + maxPowerOfi + "} of " + number);
-                number = number.divide(maxPowerOfi);
-            }
-
-            if (i.compareTo(sqrt) >= 0) {
-                break;
+                number = number.divide(i.pow(maxPowerOfi.intValue()));
+                sqrt = number.sqrt();
             }
 
             i = i.add(TWO);
         }
 
-        System.out.println("Number of prime bases found: " + factors);
+        if (factors == 0) {
+            System.out.println(input + " is prime.\n");
+        }
+        else {
+            long totalPrimeFactors = factors;
+            if (number.compareTo(ONE) == 1) {
+                totalPrimeFactors++;
+            }
+
+            System.out.println("Completed with number = " + number);
+            System.out.println(input + " is composite. Found " + totalPrimeFactors + " prime factors (" + factors
+                                + ") of which are less than or equal to floor(sqrt(" + input + ")) = "
+                                + inputSqrt + "\n");
+        }
     }
 }
