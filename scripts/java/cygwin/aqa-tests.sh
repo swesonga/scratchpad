b10337d389ed5f0ad80f191ceff5cddc7bf6f9d3
Saint Wesonga <sawesong@microsoft.com>
2023-04-24 16:43:22 -0600

    Add missing exports

--- scripts/java/cygwin/aqa-tests.sh
+++ scripts/java/cygwin/aqa-tests.sh
@@ -1,7 +1,11 @@
+# Run using this command:
+# time /cygdrive/c/repos/scratchpad/scripts/java/cygwin/aqa-tests.sh
+
 # export TEST_JDK_HOME=/cygdrive/c/java/binaries/jdk/aarch64/jdk-11.0.19+7
 # export TESTIMAGE_PATH=/cygdrive/c/java/binaries/jdk/aarch64/jdk-11.0.19+7-test-image
 export TEST_JDK_HOME=C:\\java\\binaries\\jdk\\aarch64\\jdk-11.0.19+7
 export TESTIMAGE_PATH=C:\\java\\binaries\\jdk\\aarch64\\jdk-11.0.19+7-test-image
+export USE_TESTENV_PROPERTIES=true BUILD_LIST=openjdk 
 
 mkdir -p /cygdrive/c/java/aqa/apr2023
 cd /cygdrive/c/java/aqa/apr2023
