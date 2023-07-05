d4781b0500f7353b015a46e02c143b528f43507f
Saint Wesonga <sawesong@microsoft.com>
2023-07-05 12:24:36 -0600

    Hard code an output dir for profiler artifacts

--- demos/java/FindPrimes/factorization-profile.sh
+++ demos/java/FindPrimes/factorization-profile.sh
@@ -1,20 +1,21 @@
 timestamp=`date +%Y-%m-%d_%H%M%S`
 duration=5
 profiler_path=~/java/binaries/async-profiler/async-profiler-2.9-linux-x64
+profilerOutDir=~/
 
-$profiler_path/profiler.sh -e wall -i 5ms -d $duration -o flat        -f "Factorize-wall-flat-${timestamp}.txt" jps
-$profiler_path/profiler.sh -e wall -i 5ms -d $duration -o traces      -f "Factorize-wall-traces-${timestamp}.txt" jps
-$profiler_path/profiler.sh -e wall -i 5ms -d $duration -o flat,traces -f "Factorize-wall-flat+traces-${timestamp}.txt" jps
-$profiler_path/profiler.sh -e wall -i 5ms -d $duration -o tree        -f "Factorize-wall-tree-${timestamp}.html" jps
-$profiler_path/profiler.sh -e wall -i 5ms -d $duration -o flamegraph  -f "Factorize-wall-flamegraph-${timestamp}.html" jps
+$profiler_path/profiler.sh -e wall -i 5ms -d $duration -o flat        -f "${profilerOutDir}Factorize-wall-flat-${timestamp}.txt" jps
+$profiler_path/profiler.sh -e wall -i 5ms -d $duration -o traces      -f "${profilerOutDir}Factorize-wall-traces-${timestamp}.txt" jps
+$profiler_path/profiler.sh -e wall -i 5ms -d $duration -o flat,traces -f "${profilerOutDir}Factorize-wall-flat+traces-${timestamp}.txt" jps
+$profiler_path/profiler.sh -e wall -i 5ms -d $duration -o tree        -f "${profilerOutDir}Factorize-wall-tree-${timestamp}.html" jps
+$profiler_path/profiler.sh -e wall -i 5ms -d $duration -o flamegraph  -f "${profilerOutDir}Factorize-wall-flamegraph-${timestamp}.html" jps
 
-$profiler_path/profiler.sh -e cpu -i 5ms -d $duration -o flat        -f "Factorize-cpu-flat-${timestamp}.txt" jps
-$profiler_path/profiler.sh -e cpu -i 5ms -d $duration -o traces      -f "Factorize-cpu-traces-${timestamp}.txt" jps
-$profiler_path/profiler.sh -e cpu -i 5ms -d $duration -o flat,traces -f "Factorize-cpu-flat+traces-${timestamp}.txt" jps
-$profiler_path/profiler.sh -e cpu -i 5ms -d $duration -o tree        -f "Factorize-cpu-tree-${timestamp}.html" jps
+$profiler_path/profiler.sh -e cpu -i 5ms -d $duration -o flat        -f "${profilerOutDir}Factorize-cpu-flat-${timestamp}.txt" jps
+$profiler_path/profiler.sh -e cpu -i 5ms -d $duration -o traces      -f "${profilerOutDir}Factorize-cpu-traces-${timestamp}.txt" jps
+$profiler_path/profiler.sh -e cpu -i 5ms -d $duration -o flat,traces -f "${profilerOutDir}Factorize-cpu-flat+traces-${timestamp}.txt" jps
+$profiler_path/profiler.sh -e cpu -i 5ms -d $duration -o tree        -f "${profilerOutDir}Factorize-cpu-tree-${timestamp}.html" jps
 
-$profiler_path/profiler.sh -e lock -i 5ms -d $duration -o flat        -f "Factorize-lock-flat-${timestamp}.txt" jps
-$profiler_path/profiler.sh -e lock -i 5ms -d $duration -o traces      -f "Factorize-lock-traces-${timestamp}.txt" jps
-$profiler_path/profiler.sh -e lock -i 5ms -d $duration -o flat,traces -f "Factorize-lock-flat+traces-${timestamp}.txt" jps
-$profiler_path/profiler.sh -e lock -i 5ms -d $duration -o tree        -f "Factorize-lock-tree-${timestamp}.html" jps
-$profiler_path/profiler.sh -e lock -i 5ms -d $duration -o flamegraph  -f "Factorize-lock-flamegraph-${timestamp}.html" jps
+$profiler_path/profiler.sh -e lock -i 5ms -d $duration -o flat        -f "${profilerOutDir}Factorize-lock-flat-${timestamp}.txt" jps
+$profiler_path/profiler.sh -e lock -i 5ms -d $duration -o traces      -f "${profilerOutDir}Factorize-lock-traces-${timestamp}.txt" jps
+$profiler_path/profiler.sh -e lock -i 5ms -d $duration -o flat,traces -f "${profilerOutDir}Factorize-lock-flat+traces-${timestamp}.txt" jps
+$profiler_path/profiler.sh -e lock -i 5ms -d $duration -o tree        -f "${profilerOutDir}Factorize-lock-tree-${timestamp}.html" jps
+$profiler_path/profiler.sh -e lock -i 5ms -d $duration -o flamegraph  -f "${profilerOutDir}Factorize-lock-flamegraph-${timestamp}.html" jps
