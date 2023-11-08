1baffbcbda2ba9969fa3a5c97aa83ba1f5fb2f36
Saint Wesonga <saint.wesonga@microsoft.com>
2023-11-08 10:21:31 -0700

    Fix branch for jdk21 aqa tests

--- scripts/java/cygwin/aqa-tests-jdk21.sh
+++ scripts/java/cygwin/aqa-tests-jdk21.sh
@@ -10,7 +10,7 @@ export USE_TESTENV_PROPERTIES=true BUILD_LIST=openjdk
 mkdir -p /cygdrive/c/java/aqa/2023-10/jdk21
 cd /cygdrive/c/java/aqa/2023-10/jdk21
 
-git clone --depth 1 -b v0.9.7-release https://github.com/adoptium/aqa-tests.git
+git clone --depth 1 -b v0.9.9-release https://github.com/adoptium/aqa-tests.git
 cd aqa-tests/openjdk
 
 git clone --depth 1 -q --reference-if-able $HOME/openjdk_cache -b jdk-21.0.1+12 https://github.com/adoptium/jdk21u.git openjdk-jdk
