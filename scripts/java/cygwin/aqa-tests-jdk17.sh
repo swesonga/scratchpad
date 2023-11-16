98e79a461f9e1e33c926e23f5912fd8a3da1b70a
Saint Wesonga <saint.wesonga@microsoft.com>
2023-11-16 09:51:02 -0700

    Update aqa-tests branch for testing jdk17

--- scripts/java/cygwin/aqa-tests-jdk17.sh
+++ scripts/java/cygwin/aqa-tests-jdk17.sh
@@ -10,7 +10,7 @@ export USE_TESTENV_PROPERTIES=true BUILD_LIST=openjdk
 mkdir -p /cygdrive/c/java/aqa/2023-10/jdk17
 cd /cygdrive/c/java/aqa/2023-10/jdk17
 
-git clone --depth 1 -b v0.9.7-release https://github.com/adoptium/aqa-tests.git
+git clone --depth 1 -b v0.9.9-release https://github.com/adoptium/aqa-tests.git
 cd aqa-tests/openjdk
 
 git clone --depth 1 -q --reference-if-able $HOME/openjdk_cache -b jdk-17.0.9+8 https://github.com/adoptium/jdk17u.git openjdk-jdk
@@ -20,3 +20,4 @@ cd ../
 ./compile.sh
 cd TKG
 make _jdk_security3_0
+# make _jdk_tools
