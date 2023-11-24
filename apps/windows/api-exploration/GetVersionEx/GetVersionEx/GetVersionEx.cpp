ea903442ccb7ae1bf230c1693ab2d4e4d91b00df
Saint Wesonga <saint.wesonga@microsoft.com>
2023-11-24 13:40:47 -0700

    Create app that calls GetVersionEx

---
+++ apps/windows/api-exploration/GetVersionEx/GetVersionEx/GetVersionEx.cpp
@@ -0,0 +1,28 @@
+﻿#include "GetVersionEx.h"
+#include "../../include/WindowsErrorHandling.h"
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    OSVERSIONINFO osvi;
+
+    ZeroMemory(&osvi, sizeof(OSVERSIONINFO));
+    osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+
+    // https://learn.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-3-c4996?view=msvc-170
+//#pragma warning(suppress : 4996)
+
+    // https://learn.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-getversionexw
+    if (!GetVersionEx(&osvi)) {
+        PrintError(TEXT("GetVersionEx"), GetLastError());
+        return -1;
+    }
+
+    printf("OS Major Version: %d\n", osvi.dwMajorVersion);
+    printf("OS Minor Version: %d\n", osvi.dwMinorVersion);
+    printf("OS Build Number:  %d\n", osvi.dwBuildNumber);
+
+    // return 0; // Why is this optional?
+}
