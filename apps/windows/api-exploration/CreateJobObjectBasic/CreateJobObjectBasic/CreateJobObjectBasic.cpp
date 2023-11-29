cb7e3cee1653e157138929d21ab5791bce17fe40
Saint Wesonga <saint.wesonga@microsoft.com>
2023-11-29 09:10:50 -0700

    Create app that creates a job object
    This is useful for testing observability of job creation using process monitor

---
+++ apps/windows/api-exploration/CreateJobObjectBasic/CreateJobObjectBasic/CreateJobObjectBasic.cpp
@@ -0,0 +1,23 @@
+﻿#include "CreateJobObjectBasic.h"
+#include "../../include/WindowsErrorHandling.h"
+#include <Windows.h>
+
+int main(int argc, TCHAR* argv[])
+{
+    Sleep(3000);
+    if (argc > 1) {
+        HANDLE jobHandle = CreateJobObject(NULL, TEXT("Test Job"));
+        Sleep(5000);
+        if (!jobHandle) {
+            PrintError(TEXT("CreateJobObject"), GetLastError());
+            return 1;
+        }
+        else if (GetLastError() == ERROR_ALREADY_EXISTS) {
+            printf("CreateJobObject returned handle to an existing job.\n");
+        }
+        printf("CreateJobObject completed.\n");
+    }
+
+    Sleep(3000);
+    return 0;
+}
