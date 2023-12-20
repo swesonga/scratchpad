21936662dc26cf55867908bbd01c09e29400b675
Saint Wesonga <saint.wesonga@microsoft.com>
2023-12-20 12:25:07 -0700

    List gtest VS debug arguments

--- scripts/java/tests/run-gtests.sh
+++ scripts/java/tests/run-gtests.sh
@@ -4,4 +4,13 @@ cd /d/java/ms/openjdk-jdk
 cd build/windows-x86_64-server-slowdebug/images/test/hotspot/gtest/server
 ./gtestLauncher.exe -jdk:/d/java/ms/openjdk-jdk/build/windows-x86_64-server-slowdebug/jdk
 
+# To launch the gtests from a bash command line
 # /d/java/ms/openjdk-jdk/build/windows-x86_64-server-slowdebug/images/test/hotspot/gtest/server/gtestLauncher.exe -jdk:/d/java/ms/openjdk-jdk/build/windows-x86_64-server-slowdebug/jdk
+
+# To debug the gtests in Visual Studio
+# 1. Open the properties of the project
+# 2. Go to Configuration Properties > Debugging
+# 3. Enter these values:
+#      Command:           D:\java\ms\openjdk-jdk\build\windows-x86_64-server-slowdebug\images\test\hotspot\gtest\server\gtestLauncher.exe
+#      Command Arguments: -jdk:D:\java\ms\openjdk-jdk\build\windows-x86_64-server-slowdebug\jdk
+#      Working Directory: D:\java\ms\openjdk-jdk\build\windows-x86_64-server-slowdebug\images\test\hotspot\gtest\server
