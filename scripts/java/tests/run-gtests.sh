2e5c391aac8b572af626e46cb61d6a6c2dc1b64c
Saint Wesonga <saint.wesonga@microsoft.com>
2023-12-20 11:23:13 -0700

    Add simple script to run gtests

---
+++ scripts/java/tests/run-gtests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /d/java/ms/openjdk-jdk
+cd build/windows-x86_64-server-slowdebug/images/test/hotspot/gtest/server
+./gtestLauncher.exe -jdk:/d/java/ms/openjdk-jdk/build/windows-x86_64-server-slowdebug/jdk
+
+# /d/java/ms/openjdk-jdk/build/windows-x86_64-server-slowdebug/images/test/hotspot/gtest/server/gtestLauncher.exe -jdk:/d/java/ms/openjdk-jdk/build/windows-x86_64-server-slowdebug/jdk
