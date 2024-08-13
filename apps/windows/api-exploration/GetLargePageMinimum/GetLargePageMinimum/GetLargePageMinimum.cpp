fee6188463fd02cebc35b558d99920942ae5a609
Saint Wesonga <saint.wesonga@microsoft.com>
2024-08-13 08:49:18 -0600

    Create program to display LargePageMinimum

---
+++ apps/windows/api-exploration/GetLargePageMinimum/GetLargePageMinimum/GetLargePageMinimum.cpp
@@ -0,0 +1,10 @@
+﻿#include "GetLargePageMinimum.h"
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    size_t large_page_min = GetLargePageMinimum();
+    cout << "GetLargePageMinimum: " << large_page_min;
+}
