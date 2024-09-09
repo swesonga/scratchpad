f3a62a534bfc31cd34d40e5e60f4b0d3732d8169
Saint Wesonga <saint.wesonga@microsoft.com>
2024-09-09 16:31:20 -0600

    Implement a call to mmap 1GB of physical memory

--- demos/cpp/mmap/mmap.cpp
+++ demos/cpp/mmap/mmap.cpp
@@ -1,6 +1,28 @@
+/*
+* To compile using GCC:
+*
+*     g++ -std=c++11 -o mmap mmap.cpp
+*/
+
+#include <sys/mman.h>
 #include <iostream>
+
 int main()
 {
-  std::cout << "mmap test" << std::endl;
+  std::cout << "Creating a new mapping in the virtual address space..." << std::endl;
+
+  void* address = (void*)0;
+  size_t length = (1 << 30);
+  int protection = PROT_READ | PROT_WRITE;
+  int flags = MAP_ANONYMOUS | MAP_PRIVATE | MAP_NORESERVE;
+  int fd = -1;
+  int offset = 0;
+
+  void* result = mmap((void*)address, length, protection, flags, fd, offset);
+
+  std::cout << "result: " << std::hex << result << std::endl;
+
+  std::cout << "Press any key to exit" << std::endl;
+  std::cin.get();
   return 0;
 }
