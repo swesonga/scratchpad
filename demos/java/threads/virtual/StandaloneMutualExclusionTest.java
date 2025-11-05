5292d53719217be10ee0e722f701e4f836e35273
Saint Wesonga <saint.wesonga@microsoft.com>
2025-11-05 13:23:22 -0700

    Simplify StandaloneMutualExclusionTest

--- demos/java/threads/virtual/StandaloneMutualExclusionTest.java
+++ demos/java/threads/virtual/StandaloneMutualExclusionTest.java
@@ -4,10 +4,11 @@
  */
 
 /*
-export JAVA_HOME=/c/java/binaries/jdk/aarch64/2025-10/windows-jdk25u/jdk-25.0.1+8/
-export JAVA_HOME=/c/java/binaries/jdk/aarch64/2025-09/windows-jdk25u/jdk-25+36/
+export JAVA_HOME=/c/java/binaries/jdk/aarch64/2025-09/windows-jdk25u/jdk-25+36
+export JAVA_HOME2=/c/java/binaries/jdk/aarch64/2025-10/windows-jdk25u/jdk-25.0.1+8
+export JAVA_HOME3=/c/java/forks/openjdk/jdk/build/windows-aarch64-server-slowdebug/images/jdk
 $JAVA_HOME/bin/javac StandaloneMutualExclusionTest.java
-$JAVA_HOME/bin/java StandaloneMutualExclusionTest --all
+$JAVA_HOME/bin/java StandaloneMutualExclusionTest
 */
 
 public class StandaloneMutualExclusionTest {
@@ -99,75 +100,11 @@ public class StandaloneMutualExclusionTest {
         }
     }
 
-    /**
-     * Run a smaller subset of tests for quick verification
-     */
-    static void runQuickTests() throws Exception {
-        System.out.println("Running quick mutual exclusion tests...");
-
-        // Test a few representative cases
-        int[][] testCases = {
-            {0, 2},   // Only virtual threads
-            {2, 0},   // Only platform threads  
-            {2, 2},   // Mixed
-            {4, 8},   // More virtual threads
-            {8, 4},   // More platform threads
-            {0, 16},  // Many virtual threads
-            {8, 16}   // Mixed with many threads
-        };
-
-        int testCount = 0;
-        int passCount = 0;
-
-        for (int[] testCase : testCases) {
-            int np = testCase[0];
-            int nv = testCase[1];
-            testCount++;
-
-            try {
-                testMutualExclusion(np, nv);
-                passCount++;
-            } catch (Exception e) {
-                System.err.printf("✗ FAILED: testMutualExclusion(%d, %d) - %s%n", 
-                                 np, nv, e.getMessage());
-                e.printStackTrace();
-            }
-        }
-
-        System.out.printf("%nQuick Test Results: %d/%d passed%n", passCount, testCount);
-        if (passCount != testCount) {
-            System.exit(1);
-        }
-    }
-
     public static void main(String[] args) throws Exception {
-        System.out.println("Standalone Mutual Exclusion Test");
-        System.out.println("Java Version: " + System.getProperty("java.version"));
-        System.out.println("OS: " + System.getProperty("os.name") + " " + System.getProperty("os.arch"));
-
-        // Print JVM flags that might be relevant
-        String vmArgs = System.getProperty("java.vm.name") + " " + 
-                        System.getProperty("java.vm.version");
-        System.out.println("JVM: " + vmArgs);
-
-        // Check command line arguments
-        boolean runAll = args.length > 0 && args[0].equals("--all");
-
-        if (args.length > 0 && !args[0].equals("--all") && !args[0].equals("--quick")) {
-            System.out.println("Usage: java StandaloneMutualExclusionTest [--quick|--all]");
-            System.out.println("  --quick: Run a small subset of test cases (default)");
-            System.out.println("  --all:   Run all possible test case combinations");
-            System.exit(1);
-        }
-
         try {
             long startTime = System.currentTimeMillis();
 
-            if (runAll) {
-                runAllTestCases();
-            } else {
-                runQuickTests();
-            }
+            runAllTestCases();
 
             long duration = System.currentTimeMillis() - startTime;
             System.out.printf("All tests completed successfully in %d ms%n", duration);
