87b6fc95bf1ad9ce834ab4e285519992e9e34265
Saint Wesonga <saint.wesonga@microsoft.com>
2026-01-26 16:38:14 -0700

    Add example of how to run a jtreg test on a local build

--- scripts/java/tests/run-jtreg-test.sh
+++ scripts/java/tests/run-jtreg-test.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# export OS=macosx
+# export PATHPREFIX=~
+# export JDKSRCPATH=$PATHPREFIX/java/forks/openjdk/jdk/
+# export DEBUGLEVEL=slowdebug
+# export JDKARCH=aarch64
+# export JDKBUILDPATH="${JDKSRCPATH}/build/${OS}-${JDKARCH}-server-${DEBUGLEVEL}"
+# export JDKTOTEST="${JDKBUILDPATH}/images/jdk"
+# export JTREGNATIVEPATH1="${JDKBUILDPATH}/support/test/hotspot/jtreg/native/lib"
+# export JTREGNATIVEPATH2="${JDKBUILDPATH}/support/test/jdk/jtreg/native/lib"
+# export JTREGNATIVEPATH3="${JDKBUILDPATH}/support/test/lib/native/lib"
+# export GTEST_NATIVEPATH="${JDKBUILDPATH}/images/test/hotspot/gtest"
+# export GTESTPATH="${GTEST_NATIVEPATH}/server"
+# export JTREGBINPATH=$PATHPREFIX/java/binaries/jtreg/jtreg-8.1+1
+# export TESTTORUN=test/hotspot/jtreg/compiler/c2/aarch64/TestTrampoline.java
+# ./run-jtreg-test.sh $JDKSRCPATH $JDKTOTEST $JTREGBINPATH/lib/jtreg.jar $TESTTORUN -nativepath:$JTREGNATIVEPATH1
+
 if [ $# -lt 4 ]
 then
     echo "Usage:    ./run-jtreg-test.sh openjdk-repo-path test-jdk-path jtreg-jar-path test-path test-flags"
