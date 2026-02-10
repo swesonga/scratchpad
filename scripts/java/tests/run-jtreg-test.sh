5f3f29b67bf790eab4d698fa606caf4bc2bd0297
Saint Wesonga <saint.wesonga@microsoft.com>
2026-02-10 11:59:54 -0700

    Fix iteration counter initialization

--- scripts/java/tests/run-jtreg-test.sh
+++ scripts/java/tests/run-jtreg-test.sh
@@ -49,7 +49,7 @@ if test -f $java_test ; then
     echo -e "Executing: $command"
 
     iterations=1
-    for i in $(seq 2 $iterations); do
+    for i in $(seq 1 $iterations); do
         echo -e "\n---- Iteration $i of $iterations ----"
         date
         time $command
