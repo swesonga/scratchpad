f80b43973972969412c7d088c4ac5ce7a663f0f5
Saint Wesonga <saint.wesonga@microsoft.com>
2026-03-09 10:24:51 -0600

    Write to separate files for easy comparison

--- compilers/vc/volatileflag.cpp
+++ compilers/vc/volatileflag.cpp
@@ -1,17 +1,19 @@
 /*
-* Test code for investigating behavior of the /volatile compiler option.
-*
-* - cl /c /Od /volatile:ms volatileflag.cpp
-* - dumpbin /disasm /out:volatileflag.asm volatileflag.obj
-* - dumpbin /all /out:volatileflag.txt volatileflag.obj
-*
-* - cl /c /O2 /volatile:ms /Fo"volatileflag-o2.obj" volatileflag.cpp
-* - dumpbin /disasm /out:volatileflag-o2.asm volatileflag-o2.obj
-* - dumpbin /all /out:volatileflag-o2.txt volatileflag-o2.obj
-*
-* Tool documentation:
-* - https://docs.microsoft.com/en-us/cpp/build/reference/compiler-options
-* - https://docs.microsoft.com/en-us/cpp/build/reference/out-dumpbin
+Test code for investigating behavior of the /volatile compiler option.
+
+cl /c /Od /volatile:iso /Fo"volatileflag-iso.obj" volatileflag.cpp
+cl /c /Od /volatile:ms  /Fo"volatileflag-ms.obj"  volatileflag.cpp
+dumpbin /disasm /out:volatileflag-iso.asm volatileflag-iso.obj
+dumpbin /all    /out:volatileflag-iso.ms  volatileflag-ms.obj
+
+cl /c /O2 /volatile:iso /Fo"volatileflag-iso-o2.obj" volatileflag.cpp
+cl /c /O2 /volatile:ms  /Fo"volatileflag-ms-o2.obj"  volatileflag.cpp
+dumpbin /disasm /out:volatileflag-iso-o2.asm volatileflag-iso-o2.obj
+dumpbin /all    /out:volatileflag-ms-o2.txt  volatileflag-ms-o2.obj
+
+Tool documentation:
+- https://docs.microsoft.com/en-us/cpp/build/reference/compiler-options
+- https://docs.microsoft.com/en-us/cpp/build/reference/out-dumpbin
 */
 
 #include <stdio.h>
