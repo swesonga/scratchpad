5d75d132945cb3a7cc1e0f07f33da08f42843f43
Saint Wesonga <saint.wesonga@microsoft.com>
2026-08-03 18:18:16 -0600

    Update JAVA_HOME paths

--- demos/java/Windows/GetTempPath2/GetTempPath2.java
+++ demos/java/Windows/GetTempPath2/GetTempPath2.java
@@ -1,12 +1,12 @@
 /*
-  export JAVA_HOME=/c/java/binaries/jdk/aarch64/2026-01/windows-jdk25u/jdk-25.0.2+10
-  export JAVA_HOME=/d/java/binaries/jdk/x64/2026-01/windows-jdk25u/jdk-25.0.2+10
+  export JAVA_HOME=/c/java/binaries/jdk/aarch64/2026-07/windows-jdk25u/jdk-25.0.4+7
+  export JAVA_HOME=/d/java/binaries/jdk/x64/2026-07/windows-jdk25u/jdk-25.0.4+7
   $JAVA_HOME/bin/javac GetTempPath2.java
   $JAVA_HOME/bin/java GetTempPath2
  */
 
 /*
-  set JAVA_HOME=D:/java/binaries/jdk/x64/2026-01/windows-jdk25u/jdk-25.0.2+10
+  set JAVA_HOME=D:/java/binaries/jdk/x64/2026-01/windows-jdk25u/jdk-25.0.4+7
   %JAVA_HOME%/bin/javac GetTempPath2.java
   %JAVA_HOME%/bin/java GetTempPath2
  */
